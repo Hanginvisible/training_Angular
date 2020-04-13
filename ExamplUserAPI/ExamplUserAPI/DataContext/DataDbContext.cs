a4744eec37ff7dff1fb86ae4a91e912e9a9b9df4
hthang <hanghoangmmt97@gmail.com>
2020-04-13 15:44:01 +0700

    hthang commit

---
+++ ExamplUserAPI/ExamplUserAPI/DataContext/DataDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExamplUserAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamplUserAPI.DataContext
+{
+    public class DataDbContext : DbContext
+    {
+        public static int SEQ = 0;
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base(options)
+        {
+            GetData(this);
+        }
+        public DbSet<User> Users { get; set; }
+        public void GetData(DataDbContext context)
+        {
+            if (context.Users.Any()) return;
+            var items = new List<User>();
+            for (int i = 1; i <= 30; i++)
+            {
+                items.Add(new User
+                {
+                    Id = +i,
+                    UserName = "hthang" + i,
+                    FullName = "HoangHang" + i,
+                    Gender = "Nữ",
+                    Age = 10 + i,
+                    Email = "hthang@cmc.com.vn"
+                });
+                SEQ++;
+
+            }
+            context.Users.AddRange(items);
+            context.SaveChanges();
+
+        }
+    }
+}
